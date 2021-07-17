bdb92885db210d21548303d2a2b07ad9230d7015
Leonard <leonard.techel@tuhh.de>
2021-07-18 01:50:03 +0200

    feat(grapple): Sequentially run a ton of kernels. OMG IT WORKS 🥳

--- Grapple.cuh
+++ Grapple.cuh
@@ -4,8 +4,9 @@
 /**
  * Run the Grapple model checker on the GPU
  *
+ * @param runIdx Idx of the program execution
  * @returns 0 or an error code
  */
-int runGrapple();
+int runGrapple(int runIdx);
 
 #endif
