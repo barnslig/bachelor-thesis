ba61443ed4eafd925a841982fcbe42aee872bb0f
Leonard <leonard.techel@tuhh.de>
2021-07-18 01:21:33 +0200

    feat(hashtable): Widen buckets to 32 bit. Add a,b,c parameters

--- Hashtable.cu
+++ Hashtable.cu
@@ -1,20 +1,100 @@
+#include "Hashtable.cuh"
 #include <cstring>
 
-#include "Hashtable.cuh"
+/**
+ * Mix three 32-bit values reversibly
+ *
+ * For every delta with one or two bits set, and the deltas of all three
+ * high bits or all three low bits, whether the original value of a,b,c
+ * is almost all zero or is uniformly distributed,
+ *
+ * - If mix() is run forward or backward, at least 32 bits in a,b,c
+ *   have at least 1/4 probability of changing.
+ * - If mix() is run forward, every bit of c will change between 1/3 and
+ *   2/3 of the time.  (Well, 22/100 and 78/100 for some 2-bit deltas.)
+ *
+ * mix() was built out of 36 single-cycle latency instructions in a
+ * structure that could supported 2x parallelism, like so:
+ *      a -= b;
+ *      a -= c; x = (c>>13);
+ *      b -= c; a ^= x;
+ *      b -= a; x = (a<<8);
+ *      c -= a; b ^= x;
+ *      c -= b; x = (b>>13);
+ *      ...
+ * Unfortunately, superscalar Pentiums and Sparcs can't take advantage
+ * of that parallelism.  They've also turned some of those single-cycle
+ * latency instructions into multi-cycle latency instructions.  Still,
+ * this is the fastest good hash I could find.  There were about 2^^68
+ * to choose from. I only looked at a billion or so.
+*/
+#define mix(a, b, c) \
+  {                  \
+    a -= b;          \
+    a -= c;          \
+    a ^= (c >> 13);  \
+    b -= c;          \
+    b -= a;          \
+    b ^= (a << 8);   \
+    c -= a;          \
+    c -= b;          \
+    c ^= (b >> 13);  \
+    a -= b;          \
+    a -= c;          \
+    a ^= (c >> 12);  \
+    b -= c;          \
+    b -= a;          \
+    b ^= (a << 16);  \
+    c -= a;          \
+    c -= b;          \
+    c ^= (b >> 5);   \
+    a -= b;          \
+    a -= c;          \
+    a ^= (c >> 3);   \
+    b -= c;          \
+    b -= a;          \
+    b ^= (a << 10);  \
+    c -= a;          \
+    c -= b;          \
+    c ^= (b >> 15);  \
+  }
 
-__device__ void hashtable_init(Hashtable *table)
+/**
+ * Hash a variable-length key into a 32-bit value
+ *
+ * Returns a 32-bit value.  Every bit of the key affects every bit of
+ * the return value.  Every 1-bit and 2-bit delta achieves avalanche.
+ * About 6*len+35 instructions.
+ *
+ * The best hash table sizes are powers of 2. There is no need to do
+ * mod a prime (mod is sooo slow!).  If you need less than 32 bits,
+ * use a bitmask.  For example, if you need only 10 bits, do
+ *   h = (h & hashmask(10));
+ * In which case, the hash table should have hashsize(10) elements.
+ *
+ * If you are hashing n strings (uint8_t **)k, do it like this:
+ *   for (i=0, h=0; i<n; ++i) h = hash( k[i], len[i], h);
+ *
+ * By Bob Jenkins, 1996.  bob_jenkins@burtleburtle.net.  You may use this
+ * code any way you wish, private, educational, or commercial.  It's free.
+ *
+ * See http://burtleburtle.net/bob/hash/evahash.html
+ * Use for hash table lookup, or anything where one collision in 2^^32 is
+ * acceptable.  Do NOT use for cryptographic purposes.
+ *
+ * @param k The key (the unaligned variable-length array of bytes)
+ * @param len The length of the key, counting by bytes
+ * @param initval Any 4-byte value
+ * @returns The hash
+ */
+__device__ uint32_t jenkins_hash(uint8_t *k, uint32_t length, uint32_t a, uint32_t b, uint32_t c)
 {
-  memset(table, 0, sizeof(Hashtable));
-}
-
-__device__ uint32_t hash(uint8_t *k, uint32_t length, uint32_t initval)
-{
-  uint32_t a, b, c, len;
+  uint32_t /*a, b, c, */ len;
 
   /* Set up the internal state */
   len = length;
-  a = b = 0x9e3779b9; /* the golden ratio; an arbitrary value */
-  c = initval;        /* the previous hash value */
+  // a = b = 0x9e3779b9; /* the golden ratio; an arbitrary value */
+  // c = initval;        /* the previous hash value */
 
   /*---------------------------------------- handle most of the key */
   while (len >= 12)
@@ -59,4 +139,26 @@ __device__ uint32_t hash(uint8_t *k, uint32_t length, uint32_t initval)
   mix(a, b, c);
   /*-------------------------------------------- report the result */
   return c;
+}
+
+__device__ bool Hashtable::markVisited(State *state, int a, int b, int c)
+{
+  uint32_t state_hash = jenkins_hash((uint8_t *)state, sizeof(State), a, b, c) & hashmask(HASHTABLE_CAPACITY);
+
+  /* Each hash bucket can store 32 bits, each representing whether a
+   * state is already visited or not. Thus, we have to divide the hash
+   * by 32. This also saves us a lot of memory!
+   * The modulo operation from the paper is omitted here as we mask
+   * the hash so it does not exceed our hashtable.
+   */
+  uint32_t hashed_value = state_hash / 32;
+
+  /* Determine which bit within our hash bucket represents the current
+   * state by using modulo
+   */
+  uint32_t sel = state_hash % 32;
+
+  bool is_visited = (atomicOr(&elems[hashed_value], (1 << sel)) & (1 << sel)) != 0;
+
+  return is_visited;
 }
