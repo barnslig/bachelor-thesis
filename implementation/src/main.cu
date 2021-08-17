6c15b0aa170c4dc7855d85f0eb8f8f1dd232379f
Leonard Techel <leonard.techel@tuhh.de>
2021-08-17 19:41:20 +0200

    feat(main): Make hash and runs variable using getopt cli params

--- implementation/src/main.cu
+++ implementation/src/main.cu
@@ -1,30 +1,58 @@
+#include <unistd.h>
+
 #include <cstdio>
+#include <iostream>
 #include <random>
 
 #include "CudaHelper.cuh"
 #include "Grapple.cuh"
 
 /**
- * Number of Grapple runs
- *
- * Each block is a VT and as we have 250 (kGrappleVTs) blocks per run, we just
- * divide by this.
+ * Default number of Grapple runs
  */
-// TODO does this make sense? We still find way less waypoints than expected
-constexpr int kGrappleRuns = 50000 / kGrappleVTs;
+constexpr int kDefaultRuns = 50000;
 
-int main()
+int main(int argc, char *const argv[])
 {
+  std::random_device rd;
+
+  int argRuns = kDefaultRuns;
+  int argSeed = rd(); // 1736331306
+
+  int c;
+  while ((c = getopt(argc, argv, "s:n:h")) != -1)
+  {
+    switch (c)
+    {
+    case 's':
+      argSeed = std::strtol(optarg, nullptr, 10);
+      break;
+    case 'n':
+      /* Each block is a VT and as we have 250 (kGrappleVTs) blocks per
+       * run, we just divide by this.
+       */
+      // TODO does this make sense? We still find way less waypoints than expected
+      argRuns = std::strtol(optarg, nullptr, 10) / 250;
+      break;
+    case '?':
+    case 'h':
+      std::cerr << "Usage: " << argv[0] << " [options]\n\n";
+      std::cerr << "Option        Description\n";
+      std::cerr << " -s <seed>    Seed used for hash function diversification. Default: Random number\n";
+      std::cerr << " -n <runs>    Number of Grapple runs. Default: " << kDefaultRuns << "\n";
+      std::cerr << " -h           Show this message\n";
+      exit(EXIT_FAILURE);
+    }
+  }
+
   printf("run, block, thread, state\n");
 
-  // std::random_device rd; // Will be used to obtain a seed for the random number engine
-  unsigned int seed = 1736331306; // rd(); // Use a constant seed for reproducible results
-  std::mt19937 gen(seed);         // Standard mersenne_twister_engine seeded with rd()
+  std::mt19937 gen(argSeed);
 
-  cudaStream_t stream[kGrappleRuns];
+  cudaStream_t stream[argRuns];
   int ret = 0;
 
-  for (int i = 0; i < kGrappleRuns; i += 1)
+  for (int i = 0; i < argRuns; i += 1)
   {
     /* Each Grapple run gets assigned to a different CUDA stream to achieve
      * maximum concurrency
